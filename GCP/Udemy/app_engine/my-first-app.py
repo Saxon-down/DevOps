916b6e80f1f7ba274a948a1ea6f4377285ff77b2
Garry Short <garry.short@ikea.com>
2019-02-04 13:39:09 +0100

    clean-up

--- GCP/Udemy/app_engine/my-first-app.py
+++
@@ -1,7 +0,0 @@
-import webapp2
-class HomePage(webapp2.RequestHandler):
-    def get(self):
-        self.response.headers('Content-Type') = 'text/html'
-        self.response.write('<h3>Hello, world!</h3><br/><p>This is so exciting!</p>')
-
-app = webapp2.WSGIApplication([('/', HomePage), ], debug=True)
